c0ddbd4dea4ae616088c12c01eb44fb3e710bdb8
modi-99 <modiqasim@outlook.com>
2021-12-15 09:44:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/schema/SkiftByte.java
@@ -0,0 +1,14 @@
+package com.example.schema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SkiftByte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skift_byte);
+    }
+}
